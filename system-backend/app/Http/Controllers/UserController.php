806d841dea0f56fc31ff225e5ed0325b23da3825
ItsKashishMaharjan <np03cs4a210073@heraldcollege.edu.np>
2024-02-05 14:02:38 +0545

    added get all function to contoller

--- system-backend/app/Http/Controllers/UserController.php
+++ system-backend/app/Http/Controllers/UserController.php
@@ -5,29 +5,21 @@ namespace App\Http\Controllers;
 use App\Enums\ApiStatus;
 use App\Models\User;
 use Exception;
-use Faker\Extension\Helper;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function __construct(User $user) {
-        $this->helper = new Helper()    ;
-        $this->userModel = $user;
-    }
 
     public function getAll(){
         try{
             DB::beginTransaction();
-            $user = $this->userModel->getAll();
+            $user = User::all();
             DB::commit();
-            $response = $this->helper->getApiResponse(ApiStatus::Success, "All Data Fetched", ['user'=>$user]);
-            return response()->json($response, 200);
+            return response()->json([ApiStatus::Success,'user'=>$user], 200);
         }catch(Exception $e){
             DB::rollBack();
             throw new Exception($e);
-            $response = $this->helper->getApiResponse(ApiStatus::Failure, "Failed", null);
-            return response()->json($response, 200);
+            return response()->json([ApiStatus::Failure,'message' => $e], null);
         }
     }
 }
