5cc276e4006bc80c0bd83c37bea335abd1cc35dc
ItsKashishMaharjan <np03cs4a210073@heraldcollege.edu.np>
2024-02-07 21:18:08 +0545

    added toggleFlagged route

--- system-backend/routes/api.php
+++ system-backend/routes/api.php
@@ -28,6 +28,7 @@ Route::get('/user',[UserController::class, 'getAll']);
 Route::get('/user/{id}',[UserController::class, 'getById']);
 Route::get('/user/delete/{id}',[UserController::class, 'delete']);
 Route::post('/user/toggleStatus/{id}',[UserController::class,'toggleStatus']);
+Route::post('/user/toggleFlagged/{id}',[UserController::class,'toggleFlagged']);
 
 
 ////
