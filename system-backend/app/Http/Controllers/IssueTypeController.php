0322e553fd6ddf7e8d03a78a34e5850583e48c5d
ItsKashishMaharjan <np03cs4a210073@heraldcollege.edu.np>
2024-02-14 13:57:16 +0545

    added issue tye CRUD

--- system-backend/app/Http/Controllers/IssueTypeController.php
+++ system-backend/app/Http/Controllers/IssueTypeController.php
@@ -2,64 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ApiStatus;
+use App\Http\Requests\IssueType\IssueTypeRequest;
 use App\Models\IssueType;
-use Illuminate\Http\Request;
+use Exception;
 
 class IssueTypeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function getAll(){
+        try{
+            $issueType = IssueType::all();
+            return response()->json([ApiStatus::Success,'All data fetched','issueType'=>$issueType], 200);
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], null);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function getById($id){
+        try{
+            $issueType = IssueType::findorfail($id);
+            return response()->json([ApiStatus::Success,'Id found and data fetched','issueType'=>$issueType], 200);;
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
+    }
+    public function create(IssueTypeRequest $request, IssueType $issueType){
+        try{
+            $issueType->fill($request->validated());
+            $issueType->save();
+            return response()->json([ApiStatus::Success,'Added','issueType'=>$issueType], 200);
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update(IssueTypeRequest $request, $id){
+        try{
+            $issueType = IssueType::findorfail($id);
+            $issueType->title = $request->title;
+            $issueType->description = $request->description;
+            $issueType->save();
+            return response()->json([ApiStatus::Success,'Updated','issueType'=>$issueType],200);
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(IssueType $issueType)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(IssueType $issueType)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, IssueType $issueType)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(IssueType $issueType)
-    {
-        //
+    public function delete($id){
+        try{
+            $issueType =IssueType::findorfail($id);
+            $issueType->delete();
+            return null;
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
     }
 }
