7dd81573933b7fb6784429f4c93420c1ab6625ab
ItsKashishMaharjan <np03cs4a210073@heraldcollege.edu.np>
2024-02-14 14:11:22 +0545

    added comment CRUD

--- system-backend/app/Http/Controllers/CommentController.php
+++ system-backend/app/Http/Controllers/CommentController.php
@@ -2,64 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ApiStatus;
+use App\Http\Requests\Comment\CommentRequest;
 use App\Models\Comment;
-use Illuminate\Http\Request;
+use Exception;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function getAll(){
+        try{
+            $comment = Comment::all();
+            return response()->json([ApiStatus::Success,'All data fetched','comment'=>$comment], 200);
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], null);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function getById($id){
+        try{
+            $comment = Comment::findorfail($id);
+            return response()->json([ApiStatus::Success,'Id found and data fetched','comment'=>$comment], 200);;
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
+    }
+    public function create(CommentRequest $request, Comment $comment){
+        try{
+            $comment->fill($request->validated());
+            $comment->save();
+            return response()->json([ApiStatus::Success,'Added','comment'=>$comment], 200);
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update(CommentRequest $request, $id){
+        try{
+            $comment = Comment::findorfail($id);
+            $comment->description = $request->description;
+            $comment->commented_date = $request->commented_date;
+            $comment->user_id = $request->user_id;
+            $comment->report_id = $request->report_id;
+            $comment->save();
+            return response()->json([ApiStatus::Success,'Updated','comment'=>$comment],200);
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Comment $comment)
-    {
-        //
+    public function delete($id){
+        try{
+            $comment =Comment::findorfail($id);
+            $comment->delete();
+            return null;
+        }catch(Exception $e){
+            return response()->json([ApiStatus::Failure,'message' => $e->getMessage()], 200);
+        }
     }
 }
